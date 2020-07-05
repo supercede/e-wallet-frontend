23e9f13b3ad3838dc5a16c07171c2ac711f4e1b4
Sijuade Ajagunna <mahyor.sam@gmail.com>
2020-07-05 22:10:51 +0100

    chore: create signup page template

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Provider } from 'mobx-react';
 import ReactDOM from 'react-dom';
+import { Router } from 'react-router';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/Signup/signup.jsx
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import { Button, TextField } from '@material-ui/core';
+import styled from 'styled-components';
+
+import './signup.scss';
+import { inject } from 'mobx-react';
+import ErrorMessage from '../../components/error.component';
+
+const Heading = styled.h1`
+  margin-top: 0;
+`;
+
+const FormContainer = styled.div`
+  max-width: 480px;
+  width: 100%;
+  background-color: #edf4ff;
+  padding: 30px;
+  border-radius: 5px;
+`;
+
+const FormField = styled(TextField)`
+  width: 100%;
+`;
+
+class SignUpPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      passwordConfirm: '',
+      errorMessage: null,
+    };
+  }
+
+  submit = async () => {
+    const { email, password, passwordConfirm } = this.state;
+
+    try {
+      console.log(email, password, passwordConfirm);
+    } catch (error) {
+      const errorMessage = error.response.data.message;
+      this.setState({ errorMessage });
+    }
+  };
+
+  render() {
+    const { errorMessage } = this.state;
+
+    return (
+      <div className='fullscreen-wrapper'>
+        <FormContainer>
+          <Heading>Crentech Wallets</Heading>
+          <p>
+            Share fund easily with friends and family, Create an account now.
+          </p>
+
+          {errorMessage && <ErrorMessage message={this.state.errorMessage} />}
+
+          <div>
+            <FormField
+              className='outlined-name'
+              label='email'
+              margin='dense'
+              variant='outlined'
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
+          </div>
+          <div>
+            <FormField
+              className='outlined-name'
+              label='Password'
+              margin='dense'
+              variant='outlined'
+              type='password'
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+          </div>
+          <div>
+            <FormField
+              className='outlined-name'
+              label='Confirm Password'
+              margin='dense'
+              variant='outlined'
+              type='password'
+              onChange={(e) =>
+                this.setState({ passwordConfirm: e.target.value })
+              }
+            />
+          </div>
+          <p>Passwords must be at least 8 letters long.</p>
+          <hr />
+          <div>
+            <Button
+              fullWidth
+              variant='contained'
+              color='primary'
+              onClick={this.submit}
+            >
+              SIGN UP
+            </Button>
+          </div>
+        </FormContainer>
+      </div>
+    );
+  }
+}
+
+export default SignUpPage;
